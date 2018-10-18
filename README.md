c61d868d2a969003839e811afa4e5e15acdf6138
leonmightyhive <33074855+leonmightyhive@users.noreply.github.com>
2018-10-18 14:59:49 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# vodafoneCopyGAData
+# Vodafone Copy GA Data
+
+This script copies tables from one Google Biqquery Project:Dataset to another Project:Dataset.
+It uses a list of tables from the tables.json file
+
+## Installation
+
+### Create the tables.json file
+1. Go to `https://bigquery.cloud.google.com/queries/`
+2. Click on **Compose Query**
+3. Enter the following query, replacing [PROJECT_ID].[DATASET_ID] with the source Project ID and Dataset ID:
+```sql
+SELECT table_id FROM `[PROJECT_ID].[DATASET_ID].__TABLES_SUMMARY__`
+```
+4. Click on **Show Options** and uncheck **_Use Legacy SQL_**
+5. Click on **Run Query**
+6. When the query completes, click on **Download as JSON**
+7. Rename this file to `tables.json`
+
+### Copy script files to gCloud Console
+1. Go to `https://console.cloud.google.com/`
+2. Click on the Terminal icon in the toolbar to start the console
+3. Copy and paste this command into the console to copy the files:
+```
+$ git clone
+```
+3. Create a new folder by typing the command in the console `$ mkd`
+
+`$ pip install foobar`
+
+## Usage
+
+```
+$ node app.js
+```
+
---
+++ app.js
@@ -0,0 +1,50 @@
+const tables = require("./tables.json");
+const assert = require('assert');
+const BigQuery = require('@google-cloud/bigquery');
+
+async function copyTable(srcBigquery, destBigquery, datasetId, tableId) {
+	// async function to only run one copy job at a time
+	return new Promise((resolve, reject) => {
+	srcBigquery
+		.dataset(datasetId)
+		.table(tableId)
+		.copy(destBigquery.dataset(datasetId).table(tableId))
+		.then(results => {
+			const job = results[0];
+			assert.equal(job.status.state, 'DONE');
+			const errors = job.status.errors;
+			resolve(`Job ${job.id} completed.`); // if successful return job details
+			if (errors && errors.length > 0) {
+				throw errors;
+			}
+		})
+		.catch(err => {
+			reject(err); // if errors send details to error handler
+		});
+	});
+}
+
+const main = async () => {
+	// set the source project id
+	const srcProjectId = "ga-cross-profile-settings";
+	// set the destination project id
+	const destProjectId = "silicon-badge-219006";
+	// set the dataset id
+	const datasetId = "149006597";
+	// create bigquery client for the source project
+	const srcBigquery = new BigQuery({projectId: srcProjectId});
+	// create bigquery client for the destination project
+	const destBigquery = new BigQuery({projectId: destProjectId});
+	// loop through the list of tables from the tables.json file
+	for (const table of tables) {
+		// for each table copy from the source project:dataset to the destination project:dataset
+		const result = await copyTable(srcBigquery, destBigquery, datasetId, table.table_id)
+		.then((data) => {
+			console.log(data); // log out success
+		})
+		.catch(console.error); // log out errors
+	}
+}
+if (module === require.main) {
+	main();
+}
